c53be95fe1632de9e3202436dda5c13ca4996273
theo <theo.paraskeva123@gmail.com>
2025-02-27 21:43:23 +0000

    misc

--- python/test.py
+++ python/test.py
@@ -1,18 +1,40 @@
 import sys
+import numpy as np
 sys.path.append("build/Debug")
+import matplotlib.pyplot as plt
 
 import gbm_pybind as gbm
 
 ## Load data
+params = {'n_estimators': 100, 'max_depth': 3, 'learning_rate': 0.1, "n_bins":0, 'device': "gpu"}
 
-data = gbm.load_csv("data/iris.csv")
+model = gbm.Regressor(**params)
 
-print(data.featureNames)
-## Create a model
-# params = {'n_estimators': 100, 'max_depth': 3, 'learning_rate': 0.1, 'device': "gpu"}
+
+
+
+
+
+# Create a model
 # model = gbm.Regressor(**params)
-## model = gbm.Classifier()
 
-## model.fit(X_train,y_train)
 
-## model.predict(X_test)
+### y :expects 1d array
+np.random.seed(42)
+
+X = np.linspace(0, 10, 100)
+y = 3.0 * X + np.random.randn(100) * 2
+
+
+### X :expects 2d array
+print(X.reshape(-1,1))
+# model.fit(X,y)
+
+X_test = np.linspace(3, 7, 100)
+
+
+# y_pred = model.predict(X_test)
+
+
+# mse = np.mean((y_pred - y) ** 2)
+# print("Training MSE:", mse)
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
+#include <regressor.hpp>
 #include <iostream>
-#include "dataset.hpp"
 
 int main() {
-    try {
-        Dataset data = loadCSV("../data/iris.csv");
-
-        std::cout << "Dataset Loaded" << "\n";
-
-        for (int i = 0; i <data.data.size(); i++){
-            for (int j = 0; j < data.data[i].size(); j++){
-                std::cout << data.data[i][j] << " ";
-            }
-            std::cout << "\n";
-        }
-
-
-    } catch (const std::exception &ex) {
-        std::cerr << "Error:"<< ex.what() << "\n";
-    }
-
     return 0;
 }
